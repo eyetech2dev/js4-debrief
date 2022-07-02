9a59c2423c6342fdddc80a33ba85135494afe65c
eyetech2dev <eyetech2dev@gmail.com>
2022-07-02 17:26:11 +1000

    Add solution for Q5 and Q6

--- activity1/index.js
+++ activity1/index.js
@@ -78,19 +78,34 @@ goBtn.addEventListener('click', greenLight);
 
 // 5. Change textbox border colours
 // Selectors
-
+const firstInput = document.querySelector('#firstName');
+const secondInput = document.querySelector('#lastName');
 // Function
-
+const borderColor = () => {
+  firstInput.style.borderColor = 'red';
+  secondInput.style.borderColor = 'green';
+}
 // Call the event
+btn2Name.addEventListener('click', borderColor);
 
 // 6. Validate the input [length should be more than 5 characters]
 // Selectors
-
+const validateBox = document.querySelector('#myName2');
+const errorMsg = document.querySelector('#errMsg');
 /*Function {
   //if statement
 }*/
-
+const checkLength = () => {
+  if(!validateBox.value || validateBox.value.length < 8){
+    errorMsg.style.color = 'red';
+    errorMsg.innerText = 'Length should be longer than 8';
+  } else {
+    errorMsg.style.color = 'green';
+    errorMsg.innerText = 'All good'
+  }
+}
 // Call the event
+document.querySelector('#btnSub2').addEventListener('click', checkLength);
 
 // 7.create a list of hobbies
 // Selectors
