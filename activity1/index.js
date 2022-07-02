d3ace28cb9423fb5d2fa5c5f26107071cef7fefd
eyetech2dev <eyetech2dev@gmail.com>
2022-07-02 11:44:29 +1000

    Add solution to Q1

--- activity1/index.js
+++ activity1/index.js
@@ -1,33 +1,38 @@
+console.log('This js file is connected')
 // BASIC SYNTAX
 // Selectors: 
 let elementName = document.querySelector("#elementId")
 // Functions
 function functionName() {
   // do something here
- 
+ console.log("Hello")
 }
 // Call the event with an event listener
-elementName.addEventListener("onclick", functionName);
+// elementName.addEventListener("onclick", functionName);
 
 // 1. DISPLAY NAME
 // Click the button to display your name in the box
 // (Tip: in this example, you will hard code your name within the JavaScript function, rather than use the input box to grab it).
 
 // Selectors
-
 // Function
-
 // Call the event
+
+const nameClick = document.querySelector('#myName');
+const nameClickAppear = () => {
+  nameClick.value = "eyetech2dev" 
+}
+btnName.addEventListener('click', nameClickAppear);
 
 // 2. LIGHTSWITCH
 // Switch lights on mouseover
 // When the mouse cursor is on the lightbulb image, the lighton image should show; and revert back to the lightoff image when the mouse moves off.
 
 // Selectors
-
 // Function
-
 // Call the event
+
+
 
 // 3. Double click button to display content
 // Selectors
